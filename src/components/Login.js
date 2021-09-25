ead15c03b7a11130679f1888da95d82ac21aa529
HarunSarii <harunsari14@gmail.com>
2021-09-26 02:19:19 +0300

    add Hero styled component

--- src/components/Login.js
+++ src/components/Login.js
@@ -14,7 +14,12 @@ const Login = () => {
                     <SignIn>Sign In</SignIn>
                 </div>
             </Nav>
-            <Section>Section</Section>
+            <Section>
+                <Hero>
+                    <h1>Welcome to your professional community</h1>
+                    <img src="/images/login-hero.svg" alt="hero" />
+                </Hero>
+            </Section>
         </Container>
     )
 }
@@ -88,7 +93,38 @@ const Section = styled.section`
             margin: auto;
             min-height: 0px;
         }
+    `
 
+const Hero = styled.div`
+    width: 100%;
+    h1 {
+        padding-bottom: 0;
+        width: 55%;
+        font-size: 56px;
+        color: #2977c9;
+        font-weight: 200;
+        line-height: 70px;
+        @media (max-width: 768px) {
+            text-align: center;
+            font-size: 20px;
+            width: 100%;
+            line-height: 2;
+        }
+    }
+    img {
+        /* z-index: -1; */
+        width: 700px;
+        height: 670px;
+        position: absolute;
+        bottom: -2px;
+        right: -150px;
+        @media (max-width: 768px) {
+            top: 230px;
+            width: initial;
+            position: initial;
+            height: initial;
+        } 
+    }
     `
 
 export default Login
