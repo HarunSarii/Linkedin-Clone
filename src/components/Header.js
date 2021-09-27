45cac24eb0ad10aafa8b9c4a8745ef346873554c
HarunSarii <harunsari14@gmail.com>
2021-09-27 12:24:37 +0300

    Add search-bar to Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,13 +4,25 @@ import styled from 'styled-components'
 const Header = () => {
     return (
         <Container>
-            <Content>Header</Content>
+            <Content>
+                <Logo>
+                    <a href="/home"><img src="/images/home-logo.svg" alt="home" /></a>
+                </Logo>
+                <Search>
+                    <div>
+                        <input type="text" placeholder='Search' />
+                        <SearchIcon>
+                            <img src="/images/search-icon.svg" alt="search" />
+                        </SearchIcon>
+                    </div>
+                </Search>
+            </Content>
         </Container>
     )
 }
 
 const Container = styled.div`
-    background-color: red;
+    background-color: white;
     border-bottom: 1px solid rgba(0,0,0,0.08);
     left: 0;
     padding: 0 24px;
@@ -29,5 +41,50 @@ const Content = styled.div`
     max-width: 1128px;
 
 `
+const Logo = styled.span`
+    margin-right:8px;
+    font-size: 0px;
+
+`
+
+const Search = styled.div`
+    opacity: 1;
+    flex-grow: 1;
+    position: relative;
+    &>div {
+        max-width: 280px;
+        input {
+            border: none;
+            box-shadow: none;
+            background-color: #eef3f8;
+            border-radius: 2px;
+            color: rgba(0,0,0,0.9);
+            width: 218px;
+            padding: 0 8px 0 40px;
+            line-height: 1.75;
+            font-weight: 400;
+            font-size: 14px;
+            height: 34px;
+            border-color: #dce6f1;
+            vertical-align: text-top;
+
+        }
+    }
+    `
+
+const SearchIcon = styled.div`
+        width: 40px;
+        position: absolute;
+        z-index: 1;
+        top: 10px;
+        left: 2px;
+        border-radius: 0 2px 2px 0;
+        margin: 0;
+        pointer-events: none;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    `
+
 
 export default Header
