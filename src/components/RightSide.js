e6ab543a82e5e978fac8b4ebe56c75bbd06aa5ae
HarunSarii <harunsari14@gmail.com>
2021-10-01 00:30:34 +0300

    Add FeedList and Avatar

--- src/components/RightSide.js
+++ src/components/RightSide.js
@@ -7,8 +7,28 @@ const RightSide = () => {
             <FollowCard>
                 <Title>
                     <h2>Add to your feed</h2>
-                    <img src="/images/feed-icon.svg" alt="" />
+                    <img src="/images/feed-icon.svg" alt="feed" />
                 </Title>
+                <FeedList>
+                    <li>
+                        <a>
+                            <Avatar />
+                        </a>
+                        <div>
+                            <span>#Linkedin</span>
+                            <button>Follow</button>
+                        </div>
+                    </li>
+                    <li>
+                        <a>
+                            <Avatar />
+                        </a>
+                        <div>
+                            <span>#Video</span>
+                            <button>Follow</button>
+                        </div>
+                    </li>
+                </FeedList>
             </FollowCard>
         </Container>
     )
@@ -29,7 +49,7 @@ const FollowCard = styled.div`
     padding: 12px;
 `
 const Title = styled.div`
-display: inline-flex;
+    display: inline-flex;
     align-items: center;
     justify-content: space-between;
     font-size: 16px;
@@ -37,4 +57,44 @@ display: inline-flex;
     color: rgba(0,0,0,0.6);
 `
 
+const FeedList = styled.ul`
+    margin-top: 16px;
+    li {
+        display: flex;
+        align-items: center;
+        margin: 12px 0;
+        position: relative;
+        font-size: 14px;
+        & > div {
+            display: flex;
+            flex-direction: column;
+
+        }
+        button {
+            background-color: transparent;
+            color: rgba(0,0,0,0.6);
+            box-shadow: inset 0 0 0 1px rgba(0,0,0,0.6);
+            padding: 16px;
+            align-items: center;
+            border-radius: 15px;
+            box-sizing: border-box;
+            font-weight: 600;
+            display: inline-flex;
+            justify-content: center;
+            max-height: 32px;
+            max-width: 480px;
+            text-align: center;
+            outline: none;
+        }
+    }
+    `
+const Avatar = styled.ul`
+    background-image: url("https://static-exp1.licdn.com/sc/h/1b4vl1r54ijmrmcyxzoidwmxs");
+    background-size: contain;
+    background-position: center;
+    background-repeat: no-repeat;
+    width: 48px;
+    height: 48px;
+    margin-right: 8px;
+`
 export default RightSide
