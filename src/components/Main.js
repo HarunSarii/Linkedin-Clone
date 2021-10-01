a3044a7e553e1ecdb6cbf1b834983d49825a1a55
HarunSarii <harunsari14@gmail.com>
2021-10-01 22:49:02 +0300

    Add Container and CommonCard

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,30 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 const Main = () => {
     return (
         <Container>
-            Main
+            <ShareBox>Share</ShareBox>
         </Container>
-    )
-}
+    );
+};
 
 const Container = styled.div`
+  grid-area: main;
+`;
+const CommonCard = styled.div`
+     text-align: center;
+  overflow: hidden;
+  margin-bottom: 8px;
+  background-color: #fff;
+  border-radius: 5px;
+  position: relative;
+  border: none;
+  box-shadow: 0 0 0 1px rgb(0 0 0 / 15%), 0 0 0 rgb(0 0 0 / 20%);
+`;
+const ShareBox = styled(CommonCard)`
+  display: flex;
+  flex-direction: column;
+`;
 
-`
-export default Main
+export default Main;
