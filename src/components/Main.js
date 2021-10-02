9846166f464139e746bfcfb7491ab0c2118e00ff
HarunSarii <harunsari14@gmail.com>
2021-10-03 00:11:17 +0300

    Add SharedActor

--- src/components/Main.js
+++ src/components/Main.js
@@ -41,7 +41,7 @@ const Main = () => {
                 <Article>
                     <SharedActor>
                         <a >
-                            <MoreHorizIcon />
+                            <img src="/images/user.svg" alt="" />
                             <div>
                                 <span>Title</span>
                                 <span>Info</span>
@@ -138,5 +138,55 @@ const Article = styled(CommonCard)`
 
 `
 
-const SharedActor = styled.div``
+const SharedActor = styled.div`
+    /* padding-right: 40px; */
+    flex-wrap: nowrap;
+    padding: 12px 16px 0;
+    margin-bottom: 8px;
+    align-items: center;
+    display: flex;
+    a {
+        margin-right: 12px;
+        flex-grow: 1;
+        overflow: hidden;
+        display: flex;
+        text-decoration:none;
+
+        img {
+            width: 48px;
+            height: 48px;
+        }
+
+        & > div {
+            display: flex;
+            flex-direction: column;
+            flex-grow: 1;
+            flex-basis: 0;
+            margin-left: 8px;
+            overflow: hidden;
+
+            span {
+                text-align: left;
+                &:first-child {
+                    font-size: 14px;
+                    font-weight: 700;
+                    color: rgba(0,0,0,1);                   
+                }
+
+                &:nth-child(n+1) {
+                    font-size:12px;
+                    color: rgba(0,0,0,0.6)
+                }
+            }
+        }
+    }
+    button {
+    position: absolute;
+    right: 12px;
+    top: 0;
+    background: transparent;
+    border:none;
+    outline: none;
+    }
+`
 export default Main;
