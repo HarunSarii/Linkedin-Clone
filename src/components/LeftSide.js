5d9e65bbfecf4664947afa2cceb85bf9cab15153
HarunSarii <harunsari14@gmail.com>
2022-02-26 03:33:02 +0300

    display user name dynamically

--- src/components/LeftSide.js
+++ src/components/LeftSide.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
+import { connect } from 'react-redux'
 
-const LeftSide = () => {
+const LeftSide = (props) => {
     return (
         <Container>
             <ArtCard>
@@ -9,7 +10,7 @@ const LeftSide = () => {
                     <CardBackground />
                     <a>
                         <Photo />
-                        <Link>Welcome, there!</Link>
+                        <Link>Welcome, {props.user ? props.user.displayName : "there"}!</Link>
                     </a>
                     <a>
                         <AddPhotoText>Add a Photo</AddPhotoText>
@@ -204,4 +205,10 @@ const CommunityCard = styled(ArtCard)`
     }
 `
 
-export default LeftSide
+const mapStateToProps = (state) => {
+    return {
+        user: state.userState.user,
+    }
+}
+
+export default connect(mapStateToProps)(LeftSide)
