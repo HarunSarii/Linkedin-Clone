2edc1113dcf1e48f4f953c9e2b179230b6cb01d2
harunsarii <harunsari14@gmail.com>
2022-03-04 00:18:51 +0300

    add video upload

--- src/actions/index.js
+++ src/actions/index.js
@@ -72,6 +72,19 @@ export function postArticleAPI(payload) {
                 })
             }
             );
+        } else if (payload.video){
+            db.collection("articles").add({
+                actor: {
+                    description: payload.user.email,
+                    title: payload.user.displayName,
+                    date: payload.timestamp,
+                    image: payload.user.photoURL
+                },
+                video: payload.video,
+                sharedImg: "",
+                comments: 0,
+                description: payload.description,
+            })
         }
     }
 }
